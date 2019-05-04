109f992298a9b37d4b4762dc0a9b2c03c8e03b40
Zmote <shodaime.hikarikage@gmail.com>
2019-05-04 10:29:08 +0200

    fixed a failing test

--- test/persistence/model/user.model.test.js
+++ test/persistence/model/user.model.test.js
@@ -8,7 +8,8 @@ describe("User Model Test Suite", () => {
     });
     describe("User Model", () => {
         it("should not be empty on initialization", (done) => {
-            userService.createUser("Zafer", "hacim").then(user => {
+            let userDto = {username:"Zafer", password: "hacim"};
+            userService.createUser(userDto).then(user => {
                 expect(user).to.be.not.empty;
                 done();
             });
